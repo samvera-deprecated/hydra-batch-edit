82bc88730aad77498669ae4e39e1a932e0d80552
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2012-06-15 13:32:10 -0500

    API docs for view helpers

--- app/helpers/batch_edit_helper.rb
+++ app/helpers/batch_edit_helper.rb
@@ -1,21 +1,28 @@
+# View Helpers for Hydra Batch Edit functionality
 module BatchEditHelper 
+  
   # determines if the given document id is in the batch
   def item_in_batch?(doc_id)
     session[:batch_document_ids] && session[:batch_document_ids].include?(doc_id) ? true : false
   end
 
+  # Returns true if user has activated batch edit mode in session
   def batch_edit_active?
     session[:batch_edit_state] == 'on'
   end
 
+  # Displays the batch edit tools.  Put this in your search result page template.  We recommend putting it in catalog/_sort_and_per_page.html.erb
   def batch_edit_tools
     render :partial=>'/batch_edit/tools'
   end
-
+  
+  # Displays the button that users click when they are done selecting items for a batch.  Put this in your search result page template.  We put it in catalog/index.html
   def batch_edit_continue 
     render :partial => '/batch_edit/next_page' 
   end
 
+  # Displays the button to select/deselect items for your batch.  Call this in the index partial that's rendered for each search result.
+  # @param [Hash] document the Hash (aka Solr hit) for one Solr document
   def batch_edit_select(document)
     render :partial=>'/batch_edit/add_button', :locals=>{:document=>document}
   end
